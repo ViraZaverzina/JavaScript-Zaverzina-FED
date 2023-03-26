9f6646cbb117c391ac4da263ab321d8bc06c56f9
ViraZaverzina <128915525+ViraZaverzina@users.noreply.github.com>
2023-03-26 15:42:25 +0300

    Add files via upload

---
+++ JS1/myjava.js
@@ -0,0 +1,14 @@
+let firstValue = 3;
+let secondValue = 7;
+let thirdValue = 5;
+let result = (Number(firstValue)+Number(secondValue)+Number(thirdValue))/3;
+console.log(result);
+let fourthValue = 7;
+let fifthValue = '7';
+console.log(fourthValue==fifthValue);
+console,log(fourthValue===fifthValue);
+let price = 12999;
+let priceDiscount = prompt('Price discount', '17');
+console.log(priceDiscount);
+let salePrice = Number(price)-(Number(price)/100 * Number(priceDiscount));
+console.log(salePrice);
